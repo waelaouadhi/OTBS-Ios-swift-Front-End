15020c4a5f5c18db8e77c1fe797e08a459cb5a7c
waelaouadhi <wael.aouadhi@esprit.tn>
2026-01-16 12:04:51 +0100

    Initial Commit

---
+++ GRH OTBS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GRH OTBS
+//
+//  Created by Wael on 16/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GRH OTBS/GRH_OTBSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GRH_OTBSApp.swift
+//  GRH OTBS
+//
+//  Created by Wael on 16/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct GRH_OTBSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
